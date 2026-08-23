2781c9e34eb09080600a836309b5b60cb95e5cd8
Subbu Muthiah <subbumuthiah1@gmail.com>
2026-08-22 19:39:09 -0500

    changed filepath

--- new_race.py
+++ new_race.py
@@ -155,7 +155,7 @@ def update_predictions(completed_race, year = 2026, year_races = 22, sims = 1000
         json.dump(existing, f, indent=4)
 
 if __name__ == "__main__":
-    config = json.load(open("config.json"))
+    config = json.load(open("json/config.json"))
     num_races = config["num_races"]
 
     with open("json/all_predictions.json", 'r', encoding='utf-8') as f:
